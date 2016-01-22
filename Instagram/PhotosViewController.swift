70eec8e22e8df70cef886290f41861de024157cf
varun vyas <moduvyas@gmail.com>
2016-01-22 10:43:45 -0500

    First Working Commit.

--- Instagram/PhotosViewController.swift
+++ Instagram/PhotosViewController.swift
@@ -8,13 +8,40 @@
 
 import UIKit
 
-class PhotosViewController: UIViewController {
+import MBProgressHUD
+
+import AFNetworking
+
+class PhotosViewController: UIViewController , UITableViewDataSource, UITableViewDelegate{
 
     @IBOutlet weak var tableView: UITableView!
-  
+
+    var instaImages: [NSDictionary]?
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         tableView.rowHeight = 320
+        
+        tableView.dataSource = self
+        tableView.delegate = self
+
+        actualNetworkSessionGet()
+        // Initialize a UIRefreshControl
+        let refreshControl = UIRefreshControl()
+        refreshControl.addTarget(self, action: "refreshControlAction:", forControlEvents: UIControlEvents.ValueChanged)
+        tableView.insertSubview(refreshControl, atIndex: 0)
+     
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+    func actualNetworkSessionGet() {
+        
+        MBProgressHUD.showHUDAddedTo(self.view, animated: true)
         
         let clientId = "e05c462ebd86446ea48a5af73769b602"
         let url = NSURL(string:"https://api.instagram.com/v1/media/popular?client_id=\(clientId)")
@@ -31,19 +58,54 @@ class PhotosViewController: UIViewController {
                     if let responseDictionary = try! NSJSONSerialization.JSONObjectWithData(
                         data, options:[]) as? NSDictionary {
                             NSLog("response: \(responseDictionary)")
+                            self.instaImages = responseDictionary["data"] as! [NSDictionary]
+                            self.tableView.reloadData()
                     }
                 }
+                // Hide HUD once network request comes back (must be done on main UI thread)
+                MBProgressHUD.hideHUDForView(self.view, animated: true)
         });
         task.resume()
-        
-        // Do any additional setup after loading the view.
     }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+  
+    func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        if let instaImages = instaImages {
+            return instaImages.count /* - 1 Removing the last entry as poster_path was getting null resulting in app crash . Will fix it later! :) */
+        }
+        else {
+            return 0
+        }
     }
     
+    func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
+        
+        let cell = tableView.dequeueReusableCellWithIdentifier("InstaViewCell", forIndexPath: indexPath) as! InstaViewCell
+        let movie = instaImages![indexPath.row]
+        
+        let baseUrl1 = movie["images"]!["low_resolution"]!!["url"] as! String
+        print(baseUrl1)
+        let imageUrl = NSURL(string: baseUrl1)
+        cell.instaImageView.setImageWithURL(imageUrl!)
+//
+//        if let poster_path = movie["poster_path"] as? String {
+//            let imageUrl = NSURL(string: baseUrl + poster_path)
+//            cell.instaImageView.setImageWithURL(imageUrl!)
+//        }
+        //cell.textLabel!.text = title
+        //print("row \(indexPath.row)")
+        return cell
+    }
+    
+    func tableView(tableView: UITableView, didSelectRowAtIndexPath indexPath: NSIndexPath) {
+        //NSLog("You selected cell number: \(indexPath.row)!")
+        let cell = tableView.dequeueReusableCellWithIdentifier("InstaViewCell", forIndexPath: indexPath) as! InstaViewCell
+        cell.selectionStyle = .Blue
+        
+        // Use a red color when the user selects the cell
+        let backgroundView = UIView()
+        backgroundView.backgroundColor = UIColor.cyanColor()
+        cell.selectedBackgroundView = backgroundView
+    }
 
     /*
     // MARK: - Navigation
--- Podfile
+++ Podfile
@@ -1,9 +1,12 @@
 # Uncomment this line to define a global platform for your project
-# platform :ios, '8.0'
+ platform :ios, '8.0'
 # Uncomment this line if you're using Swift
 use_frameworks!
 
 target 'Instagram' do
 pod 'AFNetworking'
+
+pod 'MBProgressHUD'
+
 end
 
